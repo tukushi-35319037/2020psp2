43206c8ff30aefaf376a009641c0cb7bd36eaf59
bb35319037 <kyo.1028.ping@gmail.com>
2020-07-01 15:51:51 +0900

    とりあえず、提出してみました。

--- k01/k01.c
+++ k01/k01.c
@@ -3,14 +3,17 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double val,double ave,int i);
+extern double var_online(double ave,double val,double square_ave,int i);
 
 int main(void)
 {
     double val;
     char fname[FILENAME_MAX];
     char buf[256];
+    int i=0;
+    double ave=0;
+    double var,square_ave,var_h;
     FILE* fp;
 
     printf("input the filename of sample:");
@@ -26,22 +29,37 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
+        i=i+1;
+        ave=ave_online( ave, val, i);
+        square_ave=(i-1)*ave*ave+val*val/i;
+        var=var_online(i,square_ave,val,ave);
 
     
 
 
 
     }
-
+var_h=i*var/(i-1);
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
 
-
+printf("平均は%f",ave);
+printf("分散は%f",var);
+printf("不変分散は%f",var_h);
     return 0;
+
 
 
 }
 
+ double ave_online(double ave ,double val,int i)
+{
+        return((i-1)*ave/i + val/i);
+
+}
+ double var_online(double ave,double val,double square_ave,int i)
+{
+    return( ((i-1)*square_ave/i+val*val/i)-((i-1)*ave/i+val/i));
+}
