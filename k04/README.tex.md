b7baa3ebc79ad26a462f6cab4508d889018de1fc
bb35319037 <kyo.1028.ping@gmail.com>
2020-07-29 16:28:54 +0900

    ｋ０４修正しました

--- k04/README.tex.md
+++ k04/README.tex.md
@@ -1,6 +1,6 @@
 # 課題4 レポート
 
-aa83988848 薗田光太郎
+bb35319037 平松響
 
 ## 課題
 
@@ -11,6 +11,36 @@ aa83988848 薗田光太郎
 この二つのファイルを読み取り，学籍番号，身長，性別の3つをメンバーとする構造体の配列で管理するとよい．
 
 ## ソースコードの説明
+1~4.ヘッダファイルを読み込む
+6~11.構造体を定義する
+13.main関数が始まる
+15~17.引数を宣言
+18.FILE構造体のポインタを宣言
+19.構造体の名前を新しい名前に変更
+21~25.ファイルを入力する
+26~30.ファイルが入らなかったら、失敗と書く
+32。空読みする
+33~37.ストリームから1行読み込み,何も入ってなかったら、それぞれの引数に入力することを繰り返す。
+38~42.クローズできなかったら、エラーがあったことを示し、プログラムを終了させる
+43~45.fnameの指すバッファに格納する
+46.文を出力する
+47.オープンして、ｆｐにポインタを代入する
+48~52.ｆｐに何も入っていなかったら、エラーがでたことを表示する
+54.iを初期化する
+55~59.ストリームから1行読み込み,何も入ってなかったら、それぞれの引数に入力することを繰り返す
+60~64.クローズできなかったら、エラーがあったことを示し、プログラムを終了させる
+65.文字を出力する
+66.特定の文字を入力させる
+67.iを初期化
+69.while文で14周suru
+71.構造体の中に入力したidと一致したら、次に進み、一致しなかったら終了。
+73.一致していたら、引数に１を代入
+74~81.idが一致していて、genderが１なら、id,male,heightを出力し、それ以外はid,female,heightを出力する。
+84.i=i+1する
+86,86.idが見つからなかったら、見つからなかったことを示す。
+88.main関数が終了
+
+
 
 
 ## 入出力結果
@@ -36,6 +66,22 @@ Which ID's data do you want? : 45313124
 ---
 No data
 ```
+$ ./k04
+input the filename of sample_heght;c:\Users\kyo\Downloads\Excel練習問題\35319037\2020psp2\sample\heightss.csv
+the filename of sample_ID:c:\Users\kyo\Downloads\Excel練習問題\35319037\2020psp2\sample\heights.csv
+input filename of sample ID:c:\Users\kyo\Downloads\Excel練習問題\35319037\2020psp2\sample\IDs.csv
+filename of sample:c:\Users\kyo\Downloads\Excel練習問題\35319037\2020psp2\sample\IDs.csv
+which ID's data do you want?
+45313124
+No data
 
+$ ./k04
+input the filename of sample_heght;c:/Users/kyo/Downloads/Excel練習問題/35319037/2020psp2/sample/heights.csv
+the filename of sample_ID:c:/Users/kyo/Downloads/Excel練習問題/35319037/2020psp2/sample/heights.csv
+input filename of sample ID:c:\Users\kyo\Downloads\Excel練習問題\35319037\2020psp2\sample\IDs.csv
+filename of sample:c:\Users\kyo\Downloads\Excel練習問題\35319037\2020psp2\sample\IDs.csv
+which ID's data do you want?
+45313125
+ID:45313125 gender:male height:167.830000
 ## 修正履歴
 
--- k04/k04.c
+++ k04/k04.c
@@ -3,55 +3,89 @@
 #include <string.h>
 #include <math.h>
 
-typedef struct 
+struct man_data
 {
-int gender,id;
-char gender[10];
+int gender;
+int id;
 double height;
-}man_data;
+};
 
 int main(void)
 {
-    int i,t_gender,input_id,n=0;
-    char fname_height[FILENAME_MAX],fname_ID[FILENAME_MAX];
-    char buf_height[256],buf_ID[254];
-    man_data man[100];
-    FILE *fp_height, *fp_ID;
+    int ID,i=0,nothing=0;
+    char fname[FILENAME_MAX];
+    char buf[256];
+    FILE *fp;
+    struct man_data data[100];
 
     printf("input the filename of sample_heght;");
-    fgets(fname_height,sizeof(fname_height),stdin);
-    fname_height[strlen(fname_height)-1]='\0';
-    printf("the filename of sample_ID;");
-    fgets(fname_ID,sizeof(fname_ID),stdin);
-    fname_ID[strlen(fname_ID)-1]='\0';
-    printf("Which ID?;");
-    scanf("%d",&input_id);
-
-    fp_height = fopen(fname_height,"r");
-
-    if(fp_height==NULL)
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1]='\0';
+    printf("the filename of sample_ID:%s\n",fname);
+    fp = fopen(fname,"r");
+    if(fp ==NULL)
     {
         fputs("file open error\n",stderr);
         exit(EXIT_FAILURE);
     }
 
-    fp_ID = fopen(fname_ID,"r");
-    if(fp_ID==NULL)
+    fgets(fname,sizeof(fname),stderr);
+   while(fgets(buf,sizeof(buf),fp) != NULL)
+   {
+       sscanf(buf,"%d,%lf",&data[i].gender,&data[i].height);
+       i++;
+   }
+    if(fclose(fp) == EOF)
     {
-        fputs("File open error\n",stderr);
+        fputs("file close error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+    printf("input filename of sample ID:");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1]='\0';
+    printf("filename of sample:%s\n",fname);
+    fp=fopen(fname,"r");
+    if(fp ==NULL)
+    {
+        fputs("file open error\n",stderr);
         exit(EXIT_FAILURE);
     }
 
-    fgets(buf_height,sizeof(buf_height),fp_height);
-while(fgets(buf_height,sizeof(buf_height),fp_height) != NULL) && fgets(buf_ID,sizeof(buf_ID),buf_ID) !=NULL)
-{
-    if(i==-1)
+    i=0;
+   while(fgets(buf,sizeof(buf),fp) != NULL)
+   {
+       sscanf(buf,"%d",&data[i].id);
+       i++;
+   }
+    if(fclose(fp) == EOF)
     {
-        i++;
-        continue;
+        fputs("file close error\n",stderr);
+        exit(EXIT_FAILURE);
     }
-    sscanf(buf,"%d,%lf",&data[i].gender,&data[i].height);
-    i++;
+    printf("which ID's data do you want?\n");
+    scanf("%d",&ID);
+    i=0;
+
+    while(i<14)
+    {
+     if(data[i].id == ID)
+     {
+         nothing =1;
+         if(data[i].gender == 1)
+         {
+             printf("ID:%d gender:male height:%lf",data[i].id,data[i].height);
+         }
+         else
+         {
+          printf("ID:%d gender:female height:%lf",data[i].id,data[i].height);
+         }
+          
+      }
+      i++;
+    }
+    if(nothing ==0)
+     {printf("No data");}
+    return 0;
 }
-}
+
 
